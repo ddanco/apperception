c82e94c8d094843b1e2eccd0f0985c7f036d30b6
Dominique <ddanco@gmail.com>
2022-01-12 15:13:13 +0100

    Don't draw overlapping shapes

--- py/stimulus.py
+++ py/stimulus.py
@@ -10,19 +10,28 @@ random.seed()
 rand_x = lambda: random.randint(-10, 10)
 rand_y = lambda: random.randint(-5, 5)
 
+polys = []
+
+def draw_if_possible(item):
+    if all (not item.overlaps(poly) for poly in polys):
+        item.draw()
+        polys.append(item)
+
 for color in ["red", "blue"]:
     for i in range(random.randint(2, 4)):
-        visual.Circle(
+        circ = visual.Circle(
             win=window,
             pos=[rand_x(), rand_y()],
             radius=0.5,
-            fillColor=color).draw()
+            fillColor=color)
+        draw_if_possible(circ)
     for j in range(random.randint(2, 4)):
-        visual.Rect(
+        rect = visual.Rect(
             win=window,
             pos=[rand_x(), rand_y()],
             size=1,
-            fillColor=color).draw()
+            fillColor=color)
+        draw_if_possible(rect)
 
 window.update()
 core.wait(2.0)
