fbd95ac53e2f9fa214b4a60b5ae79f92646d7fc6
Dominique <ddanco@gmail.com>
2022-01-13 19:38:39 +0100

    Animate image

--- py/stimulus.py
+++ py/stimulus.py
@@ -12,7 +12,7 @@ rand_y = lambda: random.randint(-5, 5)
 
 polys = []
 
-def draw_if_possible(item):
+def add_if_possible(item):
     if all (not item.overlaps(poly) for poly in polys):
         item.draw()
         polys.append(item)
@@ -23,17 +23,32 @@ for color in ["red", "blue"]:
             win=window,
             pos=[rand_x(), rand_y()],
             radius=0.5,
-            contrast=0.7,
+            contrast=0.2,
             fillColor=color)
-        draw_if_possible(circ)
+        add_if_possible(circ)
     for j in range(random.randint(2, 4)):
         rect = visual.Rect(
             win=window,
             pos=[rand_x(), rand_y()],
             size=1,
-            contrast=0.7,
+            contrast=0.2,
             fillColor=color)
-        draw_if_possible(rect)
+        add_if_possible(rect)
 
+for item in polys:
+    item.draw()
 window.update()
+core.wait(0.5)
+
+random.shuffle(polys)
+for i, _ in enumerate(polys):
+    for item in polys:
+        item.draw()
+    for k in range(i+1):
+        overlay = polys[k]
+        overlay.setContrast(0.7)
+        overlay.draw()
+    window.update()
+    core.wait(0.3)
+
 core.wait(2.0)
